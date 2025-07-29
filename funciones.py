6a7fe22399515db4c1eed919c28bc0265f22cbc4
GuidoZyl <guidozyl@gmail.com>
2025-07-29 07:33:28 -0300

    Minor changes

--- funciones.py
+++ funciones.py
@@ -1,34 +1,42 @@
 """
 Funciones del asistente virtual
 """
+
 import subprocess
 import threading
 import time
 import tkinter as tk
-#import keyboard
+
+# import keyboard
 import pyttsx3 as tts
 import pyautogui
-#import speech_recognition as sr
+
+# import speech_recognition as sr
 from PIL import Image, ImageTk
-#from pydub import AudioSegment
-#from pydub.playback import play
+
+# from pydub import AudioSegment
+# from pydub.playback import play
 import json
+
 
 def hablar(texto: str) -> None:
     """Utiliza el text to speech para decir un texto."""
     engine = tts.init()
-    engine.setProperty('rate', 145)
-    engine.setProperty('voice', 'spanish')
+    engine.setProperty("rate", 145)
+    engine.setProperty("voice", "spanish")
     engine.say(texto)
     engine.runAndWait()
+
 
 def ejecutar_app(ruta_ejecutable: str) -> None:
     """Abre la aplicación especificada."""
     subprocess.run([ruta_ejecutable], check=False)
 
+
 def reproducir_audio(audio, duracion: float) -> None:
     """Reproduce un audio"""
     pass
+
 
 def reproducir_cancion_spotify(nombre_cancion: str) -> None:
     """Reproduce la canción especificada en Spotify."""
@@ -40,7 +48,7 @@ def reproducir_cancion_spotify(nombre_cancion: str) -> None:
     # Esperar unos segundos para que Spotify se abra completamente
     time.sleep(5)
 
-    pyautogui.hotkey('ctrl', 'l')  # Seleccionar la barra de búsqueda
+    pyautogui.hotkey("ctrl", "l")  # Seleccionar la barra de búsqueda
     time.sleep(1)
 
     # Nota: el nombre de la canción debe ser tal que la primera canción que aparezca en
@@ -50,15 +58,13 @@ def reproducir_cancion_spotify(nombre_cancion: str) -> None:
     time.sleep(2)  # Esperar a que se carguen los resultados de búsqueda
 
     # Seleccionar el botón de reproducir
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('enter')
+    pyautogui.press("enter")
+    pyautogui.press("tab")
+    pyautogui.press("tab")
+    pyautogui.press("enter")
     time.sleep(0.8)
-    pyautogui.press('enter')
+    pyautogui.press("enter")
+
 
 def abrir_imagen(ruta_imagen: str) -> None:
     """Abre la imagen de la ruta especificada en pantalla completa. Se cierra al hacer click."""
@@ -78,29 +84,33 @@ def abrir_imagen(ruta_imagen: str) -> None:
     etiqueta = tk.Label(ventana, image=imagen_tk)
     etiqueta.pack(fill=tk.BOTH, expand=True)
 
-    ventana.attributes('-fullscreen', True)
+    ventana.attributes("-fullscreen", True)
 
     # Ocultar el cursor
-    ventana.config(cursor='none')
+    ventana.config(cursor="none")
 
     # Salir de la aplicación al hacer click
-    etiqueta.bind('<Button-1>', lambda event: ventana.destroy())
+    etiqueta.bind("<Button-1>", lambda event: ventana.destroy())
 
     # Mostrar la ventana en primer plano
     ventana.update_idletasks()
     ventana.lift()
-    ventana.attributes('-topmost', True)
-    ventana.after_idle(ventana.attributes, '-topmost', False)
+    ventana.attributes("-topmost", True)
+    ventana.after_idle(ventana.attributes, "-topmost", False)
 
     ventana.mainloop()
+
 
 def agregar_comando(tecla: str) -> None:
     """Agrega un comando al JSON"""
     with open("prueba_config.json", "r+", encoding="utf-8") as archivo:
         config = json.load(archivo)
 
-if __name__ == '__main__':
-    #ejecutar_app("C:\Riot Games\Riot Client\RiotClientServices.exe")
-    hilo_hablar = threading.Thread(target=hablar, args=("Hola, soy tu asistente virtual",))
+
+if __name__ == "__main__":
+    # ejecutar_app("C:\Riot Games\Riot Client\RiotClientServices.exe")
+    hilo_hablar = threading.Thread(
+        target=hablar, args=("Hola, soy tu asistente virtual",)
+    )
     hilo_hablar.start()
     reproducir_cancion_spotify("Necesito")
